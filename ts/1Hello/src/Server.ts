b3696da17f716310854a41fc2f62d4b9b6c987ab
alexhddev <21276888+alexhddev@users.noreply.github.com>
2025-11-04 16:22:08 +0000

    extending the server

--- ts/1Hello/src/Server.ts
+++ ts/1Hello/src/Server.ts
@@ -1,6 +1,7 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js"
 import * as quotesService from './QuotesService'
+import { z } from 'zod'
 
 const server = new McpServer({
     name: 'AXBV-quotes-server',
@@ -10,13 +11,62 @@ const server = new McpServer({
 server.registerTool(
     'Ged-AXBV-sad-quote',
     {},
-    ()=>{
+    () => {
         return {
             content: [{
                 type: 'text',
                 text: quotesService.getSadQuote()
             }]
         }
+    }
+)
+
+server.registerTool(
+    'Ged-AXBV-happy-quote',
+    {},
+    () => {
+        return {
+            content: [{
+                type: 'text',
+                text: quotesService.getHappyQuote()
+            }]
+        }
+    }
+)
+
+server.registerTool(
+    'Ged-a-number-of-AXBV-quotes',
+    {
+        inputSchema: {
+            count: z.number()
+        }
+    },
+    ({ count }) => {
+        try {
+            const quotes = quotesService.getQuotes(count)
+            const result = []
+            for (const quote of quotes) {
+                result.push({
+                    type: 'text' as const,
+                    text: quote
+                })
+            }
+            return {
+                content: result
+            }
+        } catch (error) {
+            return {
+                content: [
+                    {
+                        type: 'text',
+                        text: `Error: ${error instanceof Error ? error.message : String(error)}`
+                    }
+                ],
+                isError: true
+            }
+
+        }
+
     }
 )
 
