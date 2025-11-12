04643ebeaa1c6d5cd5510370f2627b10b268733c
alexhddev <21276888+alexhddev@users.noreply.github.com>
2025-11-12 09:37:20 +0000

    TS resources

--- ts/2TransportPrimitives/src/Server.ts
+++ ts/2TransportPrimitives/src/Server.ts
@@ -1,10 +1,80 @@
-import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { McpServer, ResourceTemplate } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { z } from 'zod';
+import { join } from "path";
+import { readFileSync } from "fs";
 
 export const server = new McpServer({
     name: 'Hello name MCP',
     version: '1.0.0'
 })
+
+server.registerResource(
+    'ItemPrice',
+    new ResourceTemplate(
+        'items://{id}/price',
+        { list: undefined }
+    ),
+    {
+        title: 'Item price',
+        description: 'Get the price of an item by its ID'
+    },
+    (uri, { id }) => {
+        let price = 'Item not found'
+        if (id == '42') {
+            price = '19.99 USD'
+        }
+        if (id == '7') {
+            price = '5.49 USD'
+        }
+        return {
+            contents: [
+                {
+                    uri: uri.href,
+                    text: price
+                }
+            ]
+        }
+    }
+)
+
+
+
+
+
+
+
+server.registerResource(
+    'ReadLogs',
+    'file:///data/logs.txt',
+    {
+        title: 'Read logs',
+        description: 'Read and return the contents of data/logs.txt',
+        mimeType: 'text/plain'
+    },
+    (uri) => {
+        const logPath = join(process.cwd(), 'data', 'logs.txt');
+        const logContents = readFileSync(logPath, 'utf-8');
+
+        return {
+            contents: [
+                {
+                    uri: uri.href,
+                    text: logContents
+                }
+            ]
+        }
+
+    }
+)
+
+
+
+
+
+
+
+
+
 
 server.registerTool(
     'HelloName',
@@ -17,7 +87,7 @@ server.registerTool(
     },
     async ({ name }) => {
         return {
-            content:[{
+            content: [{
                 type: 'text',
                 text: `Hello ${name}! You are awesome`
             }]
