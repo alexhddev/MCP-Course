612093ad112e4d46d07cc22ee5aceadf82e3c528
alexhddev <21276888+alexhddev@users.noreply.github.com>
2025-11-14 13:34:24 +0000

    Tool with images

--- py/3AwesomePizzaMCP/server.py
+++ py/3AwesomePizzaMCP/server.py
@@ -1,4 +1,8 @@
+import sys
 from mcp.server.fastmcp import FastMCP
+from mcp.types import TextContent, ImageContent, CallToolResult
+import requests
+import base64
 
 mcp = FastMCP("Awesome pizza MCP")
 
@@ -17,3 +21,36 @@ def get_pizza_menu():
     from api_client import get_daily_menu
     menu = get_daily_menu()
     return menu
+
+@mcp.tool(
+    name="Get pizza menu with images",
+    description="Get the menu from awesome pizza with images."
+)
+def get_pizza_menu_with_images() -> CallToolResult:
+    from api_client import get_daily_menu
+    menu = get_daily_menu()
+    content = []
+    for item in menu:
+        content.append(
+            TextContent(type="text", text=item["name"])
+        )
+        content.append(
+            TextContent(type="text", text=item["description"])
+        )
+        image_url = f'http://localhost:3000/{item["imageUrl"]}' 
+        image_response = requests.get(image_url)    
+
+        # Convert image to base64
+        img_data = base64.b64encode(image_response.content).decode('utf-8') 
+
+        content.append(
+            ImageContent(
+                type="image",
+                data=img_data,
+                mimeType="image/png"                
+            )
+        )
+
+    return CallToolResult(
+        content=content
+    )
