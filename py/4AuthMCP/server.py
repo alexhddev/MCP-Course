5ae028865f5f7e28f89a2168519ac85805beede5
alexhddev <21276888+alexhddev@users.noreply.github.com>
2025-12-02 11:45:58 +0000

    python mcp auth

--- py/4AuthMCP/server.py
+++ py/4AuthMCP/server.py
@@ -1,8 +1,11 @@
 from mcp.server.fastmcp import FastMCP
 
 from contextlib import asynccontextmanager
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 import uvicorn
+
+from lib.auth import auth_middleware
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -12,12 +15,58 @@ async def lifespan(app: FastAPI):
         
 app = FastAPI(lifespan=lifespan)
 
-mcp = FastMCP("hello-mcp")
+app.middleware("http")(auth_middleware)
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # In production, specify your actual origins
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["*"],
+)
+
+mcp = FastMCP("hello-mcp", stateless_http=True)
 
 # Hello name tool
 @mcp.tool(name="hello_name", description="Says hello to the provided name")
 def say_hello(name: str) -> str:
     return f"Heelo {name}! You are awesome"
+
+# MCP well-known endpoint
+@app.get("/.well-known/oauth-protected-resource/mcp")
+async def oauth_protected_resource_metadata():
+
+    return {
+        "authorization_servers": ["http://localhost:4000/authorize"],
+        "bearer_methods_supported": ["header", "body"],
+        "resource": "http://localhost:8000/mcp",  # url of the mcp server
+        "scopes_supported": ["mcp:tools:search:read"],
+    }
+
+# MCP well-known endpoint
+@app.get("/.well-known/oauth-protected-resource")
+async def oauth_protected_resource_metadata():
+
+    return {
+        "authorization_servers": ["http://localhost:4000/authorize"],
+        "bearer_methods_supported": ["header", "body"],
+        "resource": "http://localhost:8000/mcp",  # url of the mcp server
+        "scopes_supported": ["mcp:tools:search:read"],
+    }
+
+# OAuth authorization server metadata endpoint
+@app.get("/.well-known/oauth-authorization-server")
+async def oauth_authorization_server_metadata():
+    return {
+        "issuer": "http://localhost:4000",
+        "authorization_endpoint": "http://localhost:4000/authorize",
+        "token_endpoint": "http://localhost:4000/token",
+        "response_types_supported": ["code"],
+        "grant_types_supported": ["authorization_code"],
+        "code_challenge_methods_supported": ["S256"],
+        "scopes_supported": ["mcp:tools:search:read"],
+    }
 
 app.mount("/", mcp.streamable_http_app())
 
